742e585c3e4a733ae200597321de2f4a99d3de0b
Saya-hack <s.saya3895@gmail.com>
2019-02-03 00:41:01 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, WebView } from 'react-native';
+
+export default class MyWeb extends Component {
+  render() {
+    return (
+      <WebView
+        source={{uri: 'https://github.com/facebook/react-native'}}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
